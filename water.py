5302cee91416f60731271fc0e1710855f0006c37
root <2062320169@qq.com>
2018-06-02 17:23:04 +0800

    arch work

--- water.py
+++ water.py
@@ -1,22 +1,39 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, send_from_directory
 
 import response
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     jsonStr=response.raw_response("http://nixni.cc")
+#     return jsonStr
+
+
+# @app.route('/about')
+# def about():
+#     return 'The about page'
+
+
+@app.route('/i')
+def index2():
+    return 'aha'
+
 @app.route('/')
 def index():
-    jsonStr=response.raw_response("http://nixni.cc")
-    return jsonStr
+    return send_static('index.html')    
 
 
-@app.route('/about')
-def about():
-    return 'The about page'
+@app.route('/<path:filename>')
+def send_static(filename):
+    return send_from_directory('static',
+                               filename)
 
-
-
+# @app.route('/<path:filename>')
+# def download_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'],
+#                                filename, as_attachment=True)
 
 
 
