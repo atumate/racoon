91d951463ec5c61384456a05fb90b49f5e1fecff
root <2062320169@qq.com>
2018-06-10 00:48:59 +0800

    ajax ok

--- static/dist/main.js
+++ static/dist/main.js
@@ -1,25 +1,76 @@
 
 $(document).ready(function(){
-	var API="http://192.168.125.128:7000";
+	var API_PATH="/api"; // abs path recommned
+
+	$('#textarea-urls').val(' https://github.com \n  http://nixni.cc');
 
 	$("#btn-url-submit").on("click", function() {
+		show_loader();
 
-		//var url = $('#input-url').val();
+		urls_array=get_urls_array_from_raw($('#textarea-urls').val());
 
+		var op="getUrlsStatusCode";
+		var post_data={
+			op:op,
+			urls:urls_array
+		};
+
+
+
+		
 
 		$.ajax({
-			method: "GET",
-			url: API,
-			// dataType: "script"
+			method: "POST",
+			url: API_PATH,
+			data: JSON.stringify(post_data),
+			dataType:'json',
+			contentType: 'application/json;charset=UTF-8',
 		}).done( function(response,status,xhr) {
+			$('#response-raw').html(JSON.stringify(response))
 			console.log(response)
+			console.log(status)
+			console.log(xhr)
 		}).fail( function(e) {
-			console.log(e)
+			//console.log(e.responseText)
+			$('#response-raw').html('ERROR:<br>'+e.responseText)
+			 console.log(e)
 		});
-
 	});
 
+	$("#btn-url-submit").click();
+
+
+	console.log(is_url_with_protocal('http://nixni.cc'))
+
 });
+
+function show_loader(){
+	$('#response-raw').html('<img src="/img/index.beating-heart-preloader.svg">');
+}
+
+function get_urls_array_from_raw(raw_str){
+	console.log(raw_str)
+	arrayOfUrls = raw_str.split(/[\r\n]+/)
+	for (var i = 0; i < arrayOfUrls.length; i++) {
+		arrayOfUrls[i]=arrayOfUrls[i].replace(/^\s+|\s+$/g, '');
+	}
+	console.log(arrayOfUrls)
+
+	return arrayOfUrls;
+}
+
+
+
+
+
+
+
+
+function is_url_with_protocal(surl){
+	var reg = /^(http|https|ftp):\/\//g;
+	return surl.match(reg)?1:0;
+}
+
 
 
 
--- water.py
+++ water.py
@@ -1,40 +1,36 @@
 #!/usr/bin/env python
-from flask import Flask, send_from_directory
-
-import response
+from flask import Flask, send_from_directory, request
+from pprint import pprint
+import r_ant
+import json
+import sys
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     jsonStr=response.raw_response("http://nixni.cc")
-#     return jsonStr
-
-
-# @app.route('/about')
-# def about():
-#     return 'The about page'
-
-
-@app.route('/i')
-def index2():
-    return 'bbb'
 
 @app.route('/')
 def index():
-    return send_static('index.html')    
+    return send_static('index.html')   
 
 
+@app.route('/api', methods=['POST', 'GET'])
+def api():
+    if request.method == 'POST':
+        result=r_ant.parse_post_data(request.json);
+        return json.dumps(result,indent=4, sort_keys=True)
+
+    else:
+        jsonStr=response.raw_response("http://nixni.cc")
+        return jsonStr
+
+
+
+# static
 @app.route('/<path:filename>')
 def send_static(filename):
     return send_from_directory('static',
                                filename)
-
-# @app.route('/<path:filename>')
-# def download_file(filename):
-#     return send_from_directory(app.config['UPLOAD_FOLDER'],
-#                                filename, as_attachment=True)
-
+ 
 
 
 
